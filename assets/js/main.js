065ab3a1b75a33e674458c1087a0eee506c80b9b
MuratSettaroglu <146528855+MuratSettaroglu@users.noreply.github.com>
2024-03-16 22:13:48 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -268,3 +268,11 @@
   new PureCounter();
 
 })()
+
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
